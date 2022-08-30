25253afbdf5e2f28ced9ec2484dd90044ddb8a4c
wijae <wijaewon@kaist.ac.kr>
2022-08-30 10:35:11 +0900

    Time: 45 ms (76.06%), Space: 13.8 MB (74.32%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -8,13 +8,4 @@ class Solution:
         for i in range((n+1)//2):
             for j in range((n+1)//2):
                 if i*2 != n-1:
-                    a = matrix[i][j]
-                    b = matrix[j][n-1-i]
-                    c = matrix[n-1-i][n-1-j]
-                    d = matrix[n-1-j][i]
-                    
-                    
-                    matrix[i][j] = d
-                    matrix[j][n-1-i] = a
-                    matrix[n-1-i][n-1-j] = b
-                    matrix[n-1-j][i] = c
+                    matrix[i][j], matrix[j][n-1-i], matrix[n-1-i][n-1-j], matrix[n-1-j][i] = matrix[n-1-j][i], matrix[i][j], matrix[j][n-1-i], matrix[n-1-i][n-1-j]
